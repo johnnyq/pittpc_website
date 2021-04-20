41940369bd4237a24a203f9a24c95b94c1fcd908
Johnny <johnny@johnny-desktop.localdomain>
2021-04-20 16:28:59 -0400

    Fixed spelling error and remove rate

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -20,7 +20,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'contact@pittpc.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "PittPC Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nCompany: $company\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@pittpc.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: no-reply@pittpc.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
